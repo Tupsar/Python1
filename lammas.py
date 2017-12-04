7b2327e16b2fea81f2753942e5dabbad7d11adb9
Tupsar <34089602+Tupsar@users.noreply.github.com>
2017-12-04 09:36:16 +0200

    Add files via upload

---
+++ lammas.py
@@ -0,0 +1,15 @@
+import time
+print ("lammas elab soos")
+time.sleep(2)
+lammas=input("lammas?")
+if lammas == ("jah"):
+    print ("Tere :)")
+    time.sleep(1)
+    asukoht=input("Kuhu sa minna tahad")
+    
+else:
+  print ("tõmmake uttu")
+  time.sleep(2)
+  print (">:( >:( >:(")
+  
+
